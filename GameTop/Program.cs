ef9acbb666265d8f9f439e348ba6f382f8815816
ildemar <64756618+ildemar@users.noreply.github.com>
2020-05-05 21:37:56 -0500

    Comit com um Jogador

---
+++ GameTop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GameTop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA("RONALDO");
+            jogo.IniciarJogo();
+        }
+    }
+
+    class JogoFODA
+    {
+        private readonly string _NomeJogador;
+
+        public JogoFODA(string nome)
+        {
+            _NomeJogador = nome;
+        }
+        public void IniciarJogo ()
+        {
+            Console.Write($"{_NomeJogador} deu um passe");
+        }
+
+    }
+}
